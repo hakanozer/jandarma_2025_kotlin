fbc3eeeaa057fa7ae4f83b4485af0dfe775ec764
Hakan ÖZER <hakanozer02@gmail.com>
2025-07-21 13:01:49 +0300

    Days_1

---
+++ app/src/main/java/com/works/project/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.works.project
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var l_txtEmail: EditText
+    lateinit var l_txtPassword: EditText
+    lateinit var l_btnLogin: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        l_txtEmail = findViewById(R.id.l_txtEmail)
+        l_txtPassword = findViewById(R.id.l_txtPassword)
+        l_btnLogin = findViewById(R.id.l_btnLogin)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        l_btnLogin.setOnClickListener {
+            login()
+        }
+    }
+
+    private fun login() {
+        val email = l_txtEmail.text.toString()
+        val password = l_txtPassword.text.toString()
+        Log.d("Login", "$email : $password")
+    }
+
+}
